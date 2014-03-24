7239b24353ce863d8c96b53665b40adc2dbd5da2
tqyq <jindaodama@gmail.com>
2014-03-24 22:17:33 +0800

    update bat

--- mongo.go
+++ mongo.go
@@ -3,8 +3,6 @@ package main
 import (
 	//	"fmt"
 	//	"io"
-	//	//	"kodec"
-	//	"client"
 	//	"net"
 	//	"os"
 	//	"reflect"
--- release.bat
+++ release.bat
@@ -1,7 +1,5 @@
-set MPATH=D:\git\cms_go
-cd %MPATH%
+cd D:\git\cms_go
 taskkill /F /IM cms_go.exe /T
-rm cms_go.exe
-go fmt && go build
+go fmt && go clean && go build
 cms_go.exe
 pause
